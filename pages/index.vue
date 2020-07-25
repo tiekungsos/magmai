98a67f9ae5084dd3c7922c71a3c19f280b5f07b0
tiekung <beeze005@hotmail.com>
2020-07-26 01:29:02 +0700

    commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container-fluid section-1">
+  <Navbar />
+    <div class="container-title">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="title-scetion">
+                <p class="top">เป็นมากกว่าที่พักเพราะเราคือ</p>
+                <p class="center">“ บ้านอีกหลังของคุณ ”</p>
+                <p class="button"> @ สังขะบุรี</p>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="hotel-book">
+              <div class="header-box">
+                <p>จองห้องพัก</p>
+                
+              </div>
+              <div class="body-box">
+                  <img src="~/assets/img/EAK_1436-HDR.png" alt="">
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+  <div class="covid-po">
+      <div class="covid-po-box">
+          
+      </div>
+  </div>
+
+</div>
+
+
+</template>
+
+<script>
+export default {}
+</script>
